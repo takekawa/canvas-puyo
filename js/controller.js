44f289177e63282fcd4d9d1f179db7fdebe39b1c
Tsutomu Takekawa <takekawa@gmail.com>
2013-05-01 15:00:38 +0900

    added left rotation and pause

--- js/controller.js
+++ js/controller.js
@@ -3,7 +3,10 @@ document.body.onkeydown = function( e ) {
         37: 'left',
         39: 'right',
         40: 'down',
-        38: 'rotate'
+        38: 'rotate',
+        88: 'rotate',
+        90: 'leftRotate',
+        13: 'pauseScreen'
     };
     if ( typeof keys[ e.keyCode ] != 'undefined' ) {
         keyPress( keys[ e.keyCode ] );
